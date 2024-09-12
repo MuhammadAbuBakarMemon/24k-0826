2f2e9c7145418cb37128d004e71e9a6d5edb0f7d
Muhammad Abu Bakar Memon <muhammadabubakermemon@gmail.com>
2024-09-12 19:06:54 +0500

    Adding files along with the word document/pdf of executed screenshots

--- Week3/PF-Lab-W03-Q5.c
+++ Week3/PF-Lab-W03-Q5.c
@@ -4,13 +4,13 @@ int main()
 {
 	int SI, P, R, T;
 	printf("Please enter principal amount between PKR 100/- and PKR 1,000,000/-: \n");
-	sacnf("%d", &P);
+	scanf("%d", &P);
 	
-	printf("Please enter rate of interest between 05% and 10%: \n");
-	sacnf("%d", &R);
+	printf("Please enter rate of interest between 05 percent and 10 percent: \n");
+	scanf("%d", &R);
 
 	printf("Please enter time period between 1 year and 10 years: \n");
-	sacnf("%d", &T);
+	scanf("%d", &T);
 
 	/* This was explicitly not instructed to be done in the question
 	   I placed these conditions to ensure that no input lies outside of the specified range
@@ -26,4 +26,4 @@ int main()
 	
 	
 	return 0;
-]
+}
