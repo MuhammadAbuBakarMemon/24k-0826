227cb139d2668b19b6cda6cc77903a20fafd2ff2
MuhammadAbuBakarMemon <muhammadabubakermemon@gmail.com>
2024-10-24 15:54:32 +0500

    Add files via upload

---
+++ Week8/C_Source_Files/PF-Lab-W8-Q3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int r = 0; 
+    int c = 0;
+
+    printf("Determine the number of rows of your matrix: ");
+    scanf("%d", &r);
+    printf("Determine the number of columns of your matrix: ");
+    scanf("%d", &c);
+
+    int mat[r][c];
+
+    for (int m = 0; m <= r; m++)
+    {
+        for (int n = 0; n <= c; n++)
+        {
+            printf("Element Row Number %d ")
+        }
+    }
+
+
+    return 0;
+}
