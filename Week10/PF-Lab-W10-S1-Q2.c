b3425011195a07aa1d724ab7529465d4c1a22a09
MuhammadAbuBakarMemon <muhammadabubakermemon@gmail.com>
2024-11-21 10:20:52 +0500

    Add files via upload

--- Week10/PF-Lab-W10-S1-Q2.c
+++ Week10/PF-Lab-W10-S1-Q2.c
@@ -1,51 +1,68 @@
 #include <stdio.h>
 #include <string.h>
 
-char * rev_str(char ar[], int right, int left);
+char* r_str(char ar[], char r_ar[], int left, int size);
 
 int main(void)
 {
 
     int len = 0;
 
-    printf("Please enter the maximum number of characters that you would like in your string: \n");
+    printf("Please state the maximum number of characters you would like to store in your string: \n");
     scanf("%d", &len);
 
-    while (getchar() != '\n');
+    while(getchar() != '\n');
 
     char ray[len + 1];
 
-    printf("Please start entering your string: \n");
-    fgets(ray, sizeof(ray), stdin);
+    printf("Please enter your string: \n");
+    fgets(ray, len + 1, stdin);
     ray[strcspn(ray, "\n")] = '\0';
+    char r_ray[len + 1];
 
+    for (int m = 0; m < len + 1; m++)
+    {
+        r_ray[m] = ' ';
+    }
 
-    printf("The string before reversal is: \n");
-    fputs(ray, stdout);
-    
+    printf("The reversed string is: \n");
 
-    printf("\nThe string after reversal is: \n");
-    fputs(rev_str(ray, 0, strlen(ray) - 1), stdout);
+    r_str(ray, r_ray, 0, strlen(ray));
+
+    fputs(r_ray, stdout);
 
     return 0;
 }
 
-static int m = 0;
+//array notation
 
-char * rev_str(char ar[], int right, int left)
+// char* r_str(char ar[], char r_ar[], int left, int size)
+// {
+
+//     if (ar[left] == '\0')
+//     {
+//         r_ar[size] = '\0';
+//         return r_ar;
+//     }
+
+//     r_ar[size - left - 1] = ar[left];
+//     left++;
+//     r_str(ar, r_ar, left, size);
+// }
+
+//same thing with pointer notation
+
+char* r_str(char *ar, char *r_ar, int left, int size)
 {
-
-    if (right >= left)
+    if (*ar == '\0')
     {
-        return ar;
-    }
-    else
-    {
-        char temp = ar[right];
-        ar[right] = ar[left];
-        ar[left] = temp;
-
-        rev_str(ar, right + 1, left - 1);
+        *(r_ar+size-left-1) = '\0';
+        return r_ar;
     }
 
+    *(r_ar+size-left-1) = *ar;
+
+    left++;
+    ar++;
+    r_str(ar, r_ar, left, size);
 }
